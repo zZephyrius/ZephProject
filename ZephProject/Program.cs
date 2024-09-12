89584651b03c80179783ed85cc56e774417f76bc
zZephyrius <94636745+zZephyrius@users.noreply.github.com>
2024-09-11 23:18:48 -0400

    initialized Zeph's branch added method

--- ZephProject/Program.cs
+++ ZephProject/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("What up Yeldir");
 Console.WriteLine("Changing origin to pull for local");
+
+void SomeMethod()
+{
+    //do some stuff
+}
